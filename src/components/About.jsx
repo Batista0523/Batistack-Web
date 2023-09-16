0841dafb4c6ed308679437f1f07260fed0a3adb7
elisaul batista <elisaulbatista@pursuit.org>
2023-09-16 15:33:49 -0400

    added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,16 +6,24 @@ const About = () => {
   return (
     <div className="about">
       <h3>About</h3>
-      <p>This is the About page content.</p>
-      <div className="go-home">
-        <Link to="/">Home</Link>
-      </div>
-      <div className="go-service">
-        <Link to="/services">Services</Link>
-      </div>
-      <div className="go-project">
-        <Link to="/projects">Projects</Link>
-      </div>
+      <p>
+        Discover a world of endless possibilities in frontend software
+        engineering with our platform. We are dedicated to crafting exceptional
+        projects that push the boundaries of creativity and technology. Our team
+        is fueled by the passion to build innovative solutions that redefine the
+        digital experience.
+      </p>
+      <p>
+        Embrace the art of coding and design as we specialize in JavaScript,
+        HTML, CSS, and React. We believe in the power of seamless user
+        interactions and elegant UIs. Let us join you on your journey to create
+        web solutions that leave a lasting impression on your users.
+      </p>
+      <p>
+        Your vision is our inspiration, and your success is our priority.
+        Explore our services and let's embark on a collaborative adventure in
+        the dynamic world of frontend development.
+      </p>
     </div>
   );
 };
