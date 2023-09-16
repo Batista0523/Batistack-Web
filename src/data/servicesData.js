b0359a587b229a177f847a467189fc8e74785ce2
elisaul batista <elisaulbatista@pursuit.org>
2023-09-16 15:44:30 -0400

    added more services

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -16,13 +16,71 @@ const servicesData = [
       title: "Performance Optimization",
       description: "We improve website performance for faster and more efficient loading."
     },
+    about: {
+      title: "Mobile App Development",
+      description: "We build cross-platform and native mobile applications to reach a broader audience."
+    },
+  },
+  {
+    about: {
+      title: "E-commerce Solutions",
+      description: "We create scalable e-commerce platforms with secure payment gateways and intuitive user interfaces."
+    },
+  },
+  {
+    about: {
+      title: "Content Management Systems (CMS)",
+      description: "We develop custom CMS solutions for easy content management and website updates."
+    },
+  },
+  {
+    about: {
+      title: "Search Engine Optimization (SEO)",
+      description: "We optimize websites to rank higher on search engine results, driving more organic traffic."
+    },
+  },
+  {
+    about: {
+      title: "Progressive Web Apps (PWAs)",
+      description: "We build PWAs that provide an app-like experience, accessible directly from the web browser."
+    },
+  },
+  {
+    about: {
+      title: "Cybersecurity Consulting",
+      description: "We offer cybersecurity consultations to ensure your web applications are secure and protected against cyber threats."
+    },
+  },
+  {
+    about: {
+      title: "Cloud Integration",
+      description: "We integrate cloud solutions for efficient data storage, management, and seamless scalability."
+    },
+  },
+  {
+    about: {
+      title: "UI/UX Audits",
+      description: "We conduct comprehensive audits to evaluate and improve the user interface and experience of existing applications."
+    },
+  },
+  {
+    about: {
+      title: "Chatbot Development",
+      description: "We develop AI-powered chatbots to enhance user engagement and automate customer interactions."
+    },
+  },
+  {
+    about: {
+      title: "Augmented Reality (AR) Integration",
+      description: "We integrate AR technology to create immersive user experiences and interactive applications."
+    },
   },
   {
     projects: {
       title: "Youtube Clone",
       description: "This app allows you to search videos using the Youtube API.",
       projectLink: "https://main--velvety-sundae-768c66.netlify.app/",
-      creator: "Elisaul Batista, Erick Tolentino"
+      creator: "Elisaul Batista, Erick Tolentino , Winder Marte"
     },
   },
   {
